7ccaa0e82b39b2e70b8a67623d06bd4d37ce5abf
Sebastian Simarro <ssebastian210@gmail.com>
2021-12-01 10:47:41 -0300

    <update><cart> estado y layout ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,46 @@
 import { GetStaticProps } from "next";
-import React from "react";
+import React, { useState } from "react";
 import { Product } from "../product/types";
 import api from "../product/api";
+import { Button, Grid, Stack, Text } from "@chakra-ui/react";
 
 interface Props {
   products: Product[];
 }
 
 const IndexRoute: React.FC<Props> = ({ products }) => {
-  return <div>{JSON.stringify(products)}</div>;
+  // seteamos el estado con un array vacio que va tener un arr de Product
+  const [cart, setcart] = React.useState<Product[]>([]);
+
+  return (
+    <Stack>
+      <Grid
+        gridGap={6}
+        p={1}
+        templateColumns="repeat(auto-fill, minmax(230px, 1fr ))"
+      >
+        {products.map((product) => (
+          <Stack key={product.id} bgColor="gray.100" p={1}>
+            <Text fontWeight="semibold ">{product.title}</Text>
+            <Text>${product.price}</Text>
+            {/* mas semantico imposible...concatena el producto que acabas de clickear en el carrito. ojo al argumento del producto q me lo comi la primera vez */}
+            <Button
+              colorScheme="blue"
+              onClick={() => setcart(cart.concat(product))}
+            >
+              Agregar
+            </Button>
+          </Stack>
+        ))}
+      </Grid>
+      {/* el carrito con el bool para mostrar solo si tiene productos agregados. */}
+      {Boolean(cart.length) && (
+        <Button colorScheme="whatsapp">
+          Completar la compra ({cart.length} productos)
+        </Button>
+      )}
+    </Stack>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -17,6 +49,9 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       products,
     },
+    // COLORCHECKTODO
+    // este parametro revalida en tiempo la info cacheada (ojo q esta en seg) despues de ese tiempo dispara un req a next para que vaya al server y parsee la nueva info.
+    ////// revalidate : 10 //////
   };
 };
 
