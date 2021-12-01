c0971d9e939f79aee9c5d740fdb8ade592babf4d
Sebastian Simarro <ssebastian210@gmail.com>
2021-12-01 12:59:18 -0300

    <update><linkwame> logica y cho

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,50 @@ import { GetStaticProps } from "next";
 import React, { useState } from "react";
 import { Product } from "../product/types";
 import api from "../product/api";
-import { Button, Grid, Stack, Text } from "@chakra-ui/react";
+import { Button, Grid, Link, Stack, Text } from "@chakra-ui/react";
 
 interface Props {
   products: Product[];
+}
+// para localizar la moneda.
+function parseCurrency(value: number): string {
+  return value.toLocaleString("es-AR", {
+    style: "currency",
+    currency: "ARS",
+  });
 }
 
 const IndexRoute: React.FC<Props> = ({ products }) => {
   // seteamos el estado con un array vacio que va tener un arr de Product
   const [cart, setcart] = React.useState<Product[]>([]);
+  //para generar en base a  lo que tengamos en el carrito.
+  // inicialmente string vacio.
+  const text = React.useMemo(
+    // el use memo asegura que solo se ejecute cuando cambie la dependencia CHECK USEEFFECT, en este caso , cuando cambie el cart.
+    () =>
+      cart
+        .reduce(
+          (message, product) =>
+            // por cada element, le agrega esto.
+            message.concat(`* ${product.title} - $${product.price}\n `),
+          ``
+        )
+        // mas el total o valor final del carro
+        .concat(
+          `\nTotal:  ${cart.reduce(
+            // el 0 es con lo que inicia.
+            (total, product) => total + product.price,
+            0
+          )}`
+        ),
+    [cart]
+  );
+
+  // function handleAddToCart(product: Product) {
+  //   setcart((cart) => cart.concat(product));
+  // }
+  // onClick={() => handleAddToCart(product)}
+  // TODO SOLO SI SACO POR FUERA para limpiar.
 
   return (
     <Stack>
@@ -22,11 +57,11 @@ const IndexRoute: React.FC<Props> = ({ products }) => {
         {products.map((product) => (
           <Stack key={product.id} bgColor="gray.100" p={1}>
             <Text fontWeight="semibold ">{product.title}</Text>
-            <Text>${product.price}</Text>
+            <Text>{parseCurrency(product.price)}</Text>
             {/* mas semantico imposible...concatena el producto que acabas de clickear en el carrito. ojo al argumento del producto q me lo comi la primera vez */}
             <Button
               colorScheme="blue"
-              onClick={() => setcart(cart.concat(product))}
+              onClick={() => setcart((cart) => cart.concat(product))}
             >
               Agregar
             </Button>
@@ -35,7 +70,13 @@ const IndexRoute: React.FC<Props> = ({ products }) => {
       </Grid>
       {/* el carrito con el bool para mostrar solo si tiene productos agregados. */}
       {Boolean(cart.length) && (
-        <Button colorScheme="whatsapp">
+        <Button
+          colorScheme="whatsapp"
+          as={Link}
+          isExternal
+          // encodeURIComponent verifica de escapar correctamente por url el txt
+          href={`https://wa.me/541126125615?text=${encodeURIComponent(text)}`}
+        >
           Completar la compra ({cart.length} productos)
         </Button>
       )}
--- product/api.ts
+++ product/api.ts
@@ -22,7 +22,18 @@ export default {
               // le pasamos un obj con params, el header indica que el primer elemento de nuestro array arma y sortea nuestra tabla y armar el json correctamente.
               header: true,
               // resultado del obj parseado
-              complete: (results) => resolve(results.data as Product[]),
+              //  version anterior ====== complete: (results) => resolve(results.data as Product[]),
+              complete: (results) => {
+                // el refactoreo es para darle un cuerpo a la function
+                const products = results.data as Product[];
+                return resolve(
+                  products.map((product) => ({
+                    ...product,
+                    price: Number(product.price),
+                  }))
+                );
+              },
+
               // si hay error devuelve mensaje y no trata de parsearlo.
               error: (error) => reject(error.message),
             });
